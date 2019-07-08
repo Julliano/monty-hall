3716167434a8377fb0cf57ebb1a74ab160aa0844
Julliano <julliano.osorio@digitro.com.br>
2019-07-08 18:01:35 -0300

    this commit has no parent... Batman

---
+++ src/app.vue
@@ -0,0 +1,41 @@
+
+
+<template>
+    <div id="app">
+        <h1>Monty Hall Problem</h1>
+        <door number="1" :hasGift="false"></door>
+    </div>
+</template>
+
+<script>
+    import door from './components/door';
+    
+    export default {
+        name: 'App',
+        components: {
+            door
+        }
+    }
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+  font-family: 'Montserrat', sans-serif;
+}
+body {
+  color: #fff;
+  background: linear-gradient(to right, rgb(21, 153, 87), rgb(21, 87, 153));
+  #app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    & h1 {
+        border: 1px solid #000;
+        background-color: #0004;
+        padding: 20px;
+        margin-bottom: 60px;
+    }
+  }
+}
+</style>
---
+++ src/components/door.vue
@@ -0,0 +1,81 @@
+
+
+<template>
+    <div class="door-area">
+        <div class="door-frame">
+            <gift v-if="hasGift && open"></gift>
+        </div>    
+        <div class="door">
+            <div class="number"> {{ number }}</div>
+            <div class="knob"></div>
+        </div>
+    </div>    
+</template>
+
+<script>
+    import gift from './gift';
+    export default {
+        name: 'Door',
+        components: {
+            gift
+        },
+        props: {
+            number: {},
+            hasGift: {
+                type: Boolean
+            }
+        },
+        data() {
+            return {
+                open: false,
+                selected: false
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+  :root {
+    --door-border: 5px solid brown;
+    .door-area {
+      position: relative;
+      width: 200px;
+      height: 310px;
+      border-bottom: 10px solid #aaa;
+      margin-bottom: 20px;
+      font-size: 3rem;
+      display: flex;
+      justify-content: center;
+      .door-frame {
+        position: absolute;
+        height: 300px;
+        width: 180px;
+        border-left: var(--door-border);
+        border-top: var(--door-border);
+        border-right: var(--door-border);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+    }
+    .door {
+      position: absolute;
+      top: 5px;
+      height: 295px;
+      width: 170px;
+      background-color: chocolate;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 20px;
+      .knob {
+        height: 20px;
+        width: 20px;
+        border-radius: 10px;
+        background-color: brown;
+        align-self: flex-start;
+        margin-top: 60px;
+      }
+    }
+  }
+</style>
